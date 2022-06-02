fd8b5ecf35c787d265651023387a0f520e5fd233
MSKteran <40267591+MSKteran@users.noreply.github.com>
2022-06-02 18:46:41 +0300

    Update ex5.cpp

--- ex5.cpp
+++ ex5.cpp
@@ -8,18 +8,23 @@ void Prime(int n)
     int check = 0;
     vector <int> list;
     list.push_back(2);
-    for (int i = 3; i < n * n; i = i + 2) {
-        for (int j : list) {
-            if (i % j == 0) {
+    for (int i = 3; i < n * n; i = i + 2) 
+    {
+        for (int j : list) 
+        {
+            if (i % j == 0) 
+            {
                 ++check;
             }
         }
-        if (check == 0) {
+        if (check == 0) 
+        {
             list.push_back(i);
             cout << i << " ";
         }
         check = 0;
-        if (list.size() == n) {
+        if (list.size() == n) 
+        {
             break;
         }
     }
@@ -32,8 +37,9 @@ int main()
     cout << "Введите количество простых чисел: " << endl;
     cin >> n;
     cout << 2 << " ";
-    if (n>1) {
-            Prime(n-1);
-        }
+    if (n>1) 
+    {
+        Prime(n-1);
+    }
     return 0;
 }
